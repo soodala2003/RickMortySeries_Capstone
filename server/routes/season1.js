a330b47bf7684d806fb96539f559864ec1577edf
soodala2003 <soodala78@gmail.com>
2025-01-05 01:30:28 -0500

    updated routes

--- server/routes/season1.js
+++ server/routes/season1.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import Season1 from "../models/Season1.js";
+import Reviews from "../models/Reviews.js";
 
 const router = express.Router();
 
@@ -29,9 +30,20 @@ router.get("/:id", async (req, res) => {
     res.status(200).json({ success: true,  data: episode });
   } catch (error) {
     console.log("Error in fetching Episode: ", error.message);
-    res.status(404).json({ success: false, message: "Episode not found" });
+    //res.status(404).json({ success: false, message: "Episode not found" });
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 });
 
+// Get all reviews of episode1 in season1
+router.get("/episode1/reviews", async (req, res) => {
+  try {
+    const reviews = await Reviews.find({});
+    res.status(200).json({ success: true,  data: reviews });
+  } catch (error) {
+    console.log("Error in fetching reviews: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 export default router;
